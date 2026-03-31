31a2f30d341c0526788893b80518037f43392dc9
ANYAOGU .C. ZABDIEL <160980730+fwesh001@users.noreply.github.com>
2026-03-31 16:39:22 +0100

    feat: add material-icons-extended dependency for enhanced icon support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
